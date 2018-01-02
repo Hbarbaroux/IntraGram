9d74b7cb3cac97b8212edab13e807c84fc09b30d
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2018-01-03 00:30:52 +0100

    Amélioration du menu

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(function() {
 
 	$(".gallery").css("display", "");
+	$(".buttonGallery").addClass("active");
 	$("section, header").not(".gallery").css("display", "none");
 
-	$("button").click(function(event) {
-			var target = $( event.target );
-			var displayedClass = target.html().toLowerCase();
-			var tab = $("."+displayedClass);
-			for (i in tab) {
-				console.log(i);
-			}
-			$("."+displayedClass).css("display", "");
-			$("section, header").not("."+displayedClass).css("display", "none");
+	$("#button").click(function() {
+		$("#button, #nav, section, header").toggleClass("visible");
+	});
+
+	$("button").click(function() {
+		var displayedClass = $(this).html().toLowerCase();
+		if (! $(this).hasClass("active")) {
+			$("button").removeClass("active");
+			$(this).addClass("active");
+			$("i").removeClass("active");
+			$("i."+$(this).attr("class").split(' ')[0]).addClass("active");
+		}
+		$("."+displayedClass).css("display", "");
+		$("section, header").not("."+displayedClass).css("display", "none");
 	});
 
 });
