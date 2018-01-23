4464b1affed597fe2963e8bfd5d32eacf9e9314e
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2018-01-23 11:46:50 +0100

    Ajout de l'option Sign Up sur la page d'accueil

---
+++ js/start_script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	$(document).on('click', 'span', function() {
+		$("#log-in").css({"display" : "none"});
+		$("p").css({"display" : "none"});
+		$("#sign-up").css({"display" : "block"});
+	});
+
+	var email = document.getElementById("email") ,confirm_email = document.getElementById("email2");
+
+	function validateEmail() {
+		if(email.value != confirm_email.value) {
+			confirm_email.setCustomValidity("Emails Don't Match");
+		} else {
+			confirm_email.setCustomValidity('');
+		}
+	}
+
+	email.onchange = validateEmail;
+	confirm_email.onkeyup = validateEmail;
+
+	var password = document.getElementById("signPassword") ,confirm_password = document.getElementById("signPassword2");
+
+	function validatePassword() {
+		if(password.value != confirm_password.value) {
+			confirm_password.setCustomValidity("Passwords Don't Match");
+		} else {
+			confirm_password.setCustomValidity('');
+		}
+	}
+
+	password.onchange = validatePassword;
+	confirm_password.onkeyup = validatePassword;
+});
